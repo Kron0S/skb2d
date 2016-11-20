6c1a99cddfe701858b25a3214edaa45d8fb498e3
Kron0S <kron00000s@gmail.com>
2016-11-20 22:40:02 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors';
+import canonize from './canonize';
+
+const app = express();
+app.use(cors());
+app.get('/', async (req, res) => {
+	try {
+		const color = req.query.color || false;
+		if (!color) {return res.send('Invalid color');}
+    return res.send(canonize(unescape(color)));
+	} catch (err) {
+		console.log(err);
+		return res.send('Invalid color');
+	}
+});
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000!');
+});
